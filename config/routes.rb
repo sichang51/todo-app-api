9afd1abf6c6c9cbab79c33c03241f534c743c38c
sichang51 <99237899+sichang51@users.noreply.github.com>
2024-02-13 17:43:49 -0800

    Add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "/users/:id" => "users#show"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#destroy"
+
+  post "/sessions" => "sessions#create"
 end
